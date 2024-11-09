e70882953a98458755575103dd39d9fb65a9202b
Gladyson Rhuan <gladysonr04@gmail.com>
2024-11-09 19:18:06 -0300

    funcao novo funcionario no controller

--- src/main/java/com/loja/controller/ClientesController.java
+++ src/main/java/com/loja/controller/ClientesController.java
@@ -5,17 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.loja.dto.RequisicaoNovoCliente;
 import com.loja.model.Clientes;
+import com.loja.repository.ClientesRepository;
 import com.loja.service.ClientesService;
-
 
 @Controller
 public class ClientesController {
 
     @Autowired
     private ClientesService clientesService;
+
+    @Autowired
+    private ClientesRepository clientesRepository;
 
     @GetMapping("/api/clientes")
     public List<Clientes> clientes() {
@@ -31,10 +36,13 @@ public class ClientesController {
         mv.addObject("clientes", clientes);
 
         return mv;
-
     }
-    
-    
 
+    @PostMapping("/clientes/adicionar")
+    public String criar(RequisicaoNovoCliente requisicao) {
+        Clientes cliente = requisicao.toClientes();
+        this.clientesRepository.save(cliente);
+        return "redirect:/clientes";
+    }
 
 }
--- src/main/java/com/loja/controller/FuncionariosController.java
+++ src/main/java/com/loja/controller/FuncionariosController.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.loja.dto.RequisicaoNovoFuncionario;
 import com.loja.model.Funcionarios;
+import com.loja.repository.FuncionariosRepository;
 import com.loja.service.FuncionariosService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class FuncionariosController {
 
     @Autowired
     private FuncionariosService funcionariosService; // Injetando o serviço
+
+    @Autowired
+    private FuncionariosRepository funcionariosRepository;
 
     @GetMapping("/api/funcionarios")
     public List<Funcionarios> getAllFuncionarios() {
@@ -28,5 +36,12 @@ public class FuncionariosController {
         ModelAndView mv = new ModelAndView("funcionarios/index");
         mv.addObject("funcionarios", funcionarios);
         return mv;
+    }
+
+    @PostMapping("/funcionarios/adicionar")
+    public String criar(RequisicaoNovoFuncionario requisicao) {
+        Funcionarios funcionario = requisicao.toFuncionario();
+        this.funcionariosRepository.save(funcionario);
+        return "redirect:/funcionarios";
     }
 }
